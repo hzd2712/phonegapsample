4813d426762a93992fa7bb2505f000e38c242100
Zedong Hou <h_z_d1987@126.com>
2014-05-19 18:07:08 +0800

    Using the Location API

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -1,7 +1,11 @@
 var EmployeeView = function(employee) {
 
 	this.initialize = function() {
+		var self = this;
 		this.el = $('<div/>');
+		this.el.on('click', '.add-location-btn', function(event) {
+			self.addLocation(event);
+		});
 	};
 	this.initialize();
 
@@ -10,6 +14,20 @@ var EmployeeView = function(employee) {
 		return this;
 	};
 
+
+	this.addLocation = function(event) {
+		event.preventDefault();
+		console.log("addLocation");
+
+		navigator.geolocation.getCurrentPosition(
+			function(position) {
+				$('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
+			},
+			function() {
+				alert('Error getting location');
+			});
+		return false;
+	};
 };
 
 EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
